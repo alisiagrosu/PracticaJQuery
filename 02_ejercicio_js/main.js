7da1cc11fc7f7bb8097a611ddc5b33af9b8a7a85
Alisia Grosu <alisiagrosu@gmail.com>
2018-03-09 23:12:43 +0100

    Ejercicio 2

---
+++ 02_ejercicio_js/main.js
@@ -0,0 +1,15 @@
+$("Document"). ready(function(){
+    $("#mensaje_img1").hide()
+});
+
+
+$("#imagen1").hover (function(){
+    console.log(
+        $("#mensaje_img1").show()
+    );
+
+}, function(){
+    console.log(
+        $("#mensaje_img1").hide()
+    );
+});
